a385854316aac630ca29d71a47493c0c9c29b124
Vera Simon <vera.simon01@gmail.com>
2019-03-04 14:18:24 -0800

    Graph Added baseline content from lecture, and updated the queue and stack refs to use collections.deque for brevity.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,8 +2,67 @@
 Simple graph implementation
 """
 
+from collections import deque
+
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set()
+
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist")
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+        else:
+            raise IndexError("That vertex does not exist")
+
+    def bft(self, starting_vertex_id):
+        # Create an empty queue
+        q = deque()
+        # Create an empty set of visited vertices
+        visited = set()
+        # Put the starting vertex in our Queue
+        q.append(starting_vertex_id)
+        # While the queue is not empty....
+        while len(q) > 0:
+            # Dequeue the first node from the queue
+            v = q.popleft()
+            # If that node has not been visted...
+            if v not in visited:
+                # Mark it as visited
+                # print(v)
+                visited.add(v)
+                # Then, put all of it's children into the queue
+                for neighbor in self.vertices[v]:
+                    q.append(neighbor)
+
+    def dft(self, starting_vertex_id):
+        # Create an empty stack
+        s = deque()
+        # Create an empty set of visited vertices
+        visited = set()
+        # Put the starting vertex in our Stack
+        s.append(starting_vertex_id)
+        # While the stack is not empty....
+        while len(s) > 0:
+            # Pop the top node from the stack
+            v = s.pop()
+            # If that node has not been visted...
+            if v not in visited:
+                # Mark it as visited
+                # print(v)
+                visited.add(v)
+                # Then, put all of it's children into the stack
+                for neighbor in self.vertices[v]:
+                    s.append(neighbor)
